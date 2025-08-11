6e55a1a4f3c5208c00dd7abd240c0396126122c2
Shaheen <shn.sdq29@gmail.com>
2025-08-12 01:07:37 +0530

    added connection model

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import {inngest, functions} from './inngest/index.js'
 import {serve} from 'inngest/express'
 import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -15,7 +16,8 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/',(req,res)=> res.send("Server is runnig"))
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
