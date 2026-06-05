44bdcf6065cd5d99e970e967b56d7dbcaf59a9be
Shaheen <shn.sdq29@gmail.com>
2026-06-05 22:33:56 +0530

    story modal

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
 import moment from 'moment'
+import StoryModal from './StoryModal'
 
 const StoriesBar = () => {
 
   const [stories, setStories] = useState([])
+  const [showModal, setShowModal] = useState(false)
+  const [viewStory, setViewStory] = useState(null)
 
   const fetchStories = async ()=>{
     setStories(dummyStoriesData)
@@ -20,7 +23,7 @@ const StoriesBar = () => {
 
       <div className='flex gap-4 pb-5'>
         {/* Add a Story Card */}
-        <div className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white'>
+        <div onClick={()=>setShowModal(true)} className='rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white'>
         <div className='h-full flex flex-col items-center justify-center p-4'>
           <div className='size-10 bg-indigo-500 rounded-full flex items-center justify-center mb-3'>
             <Plus className='w-5 h-5 text-white'/>
@@ -52,7 +55,10 @@ const StoriesBar = () => {
           ))
         }
       </div>
-
+{/* Add a Story Modal */}
+{
+showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories} />
+}
     </div>
   )
 }
