a3f89d4bf2c8dc280a1b3a6637b019a3d4990603
Shaheen <shn.sdq29@gmail.com>
2026-06-09 23:16:06 +0530

    commit

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import {BadgeCheck} from 'lucide-react'
+import moment from 'moment'
 
-const PostCard = () => {
+const PostCard = ({post}) => {
+
+  const postWithHashtags = post.content.replace(/(#\w+)/g, '<span class="text-indigo-600">$1</span>')
+
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
       {/* User info */}
       <div className='inline-flex items-center gap-3 cursor-pointer'>
         <img src={post.user.profile_picture} alt="" className='w-10 h-10 rounded-full shadow' />
-
+        <div>
+        <div className='flex items-center space-x-1'>
+            <span>{post.user.full_name} </span>
+            <BadgeCheck className='w-4 h-4 text-blue-500' />
+        </div>
+          <div className='text-gray-500 text-sm'>@{post.user.username} . {moment(post.createdAt).fromNow()} </div>
       </div>
+    </div>
+    {/* Content */}
+     {post.content && <div className='text-gray-800 text-sm whitespace-pre-line' dangerouslySetInnerHTML={{__html:postWithHashtags}}/>}
+
+     {/* Images */}
+     <div className='grid grid-cols-2 gap-2'>
+      {post.image_urls.map((img,index)=>(
+        <img src={img} key={index} className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`} alt="" />
+      ))}
+     </div>
+
+
+{/* Actions */}
+<div className='flex '>
+
+</div>
+
     </div>
   )
 }
