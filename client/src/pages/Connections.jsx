52487973c830ac76ec5cfe52287cf3a46a146dc3
Shaheen <shn.sdq29@gmail.com>
2026-06-12 21:17:33 +0530

    commit

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {Users, UserPlus, UserCheck, UserRoundPen, MessageSquare} from 'lucide-react'
+import {
+   dummyConnectionsData as connections,
+   dummyFollowersData as followers,
+   dummyFollowingData as following,
+   dummyPendingConnectionsData as pendingConnections
+   } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
 
 const Connections = () => {
+
+  const navigate = useNavigate()
+
+  const dataArray = [
+    {}
+  ]
+
   return (
-    <div>Connections</div>
+    <div>
+
+    </div>
   )
 }
 
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { dummyConnectionsData } from '../assets/assets'
-import { MessageSquare } from 'lucide-react'
+import { Eye, MessageSquare } from 'lucide-react'
+import {useNavigate} from 'react-router-dom'
 
 const Messages = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='min-h-screen relative bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
@@ -24,9 +28,15 @@ const Messages = () => {
               </div>
 
               <div className='flex flex-col gap-2 mt-4'>
-                <button>
+
+                <button onClick={()=> navigate(`/messages/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
                   <MessageSquare className='w-4 h-4' />
                 </button>
+
+                <button onClick={()=> navigate(`/profile/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
+                  <Eye className='w-4 h-4' />
+                </button>
+
               </div>
             </div>
           ))}
