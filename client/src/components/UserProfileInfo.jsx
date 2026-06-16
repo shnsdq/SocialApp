ed4db9586627443cf9a2e271f1d8c5001aa7f57b
Shaheen <shn.sdq29@gmail.com>
2026-06-16 21:17:54 +0530

    commit

---
+++ client/src/components/UserProfileInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserProfileInfo = ({user, posts, profileId, setShowEdit}) => {
+  return (
+    <div>UserProfileInfo</div>
+  )
+}
+
+export default UserProfileInfo
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
+import { dummyPostsData, dummyUserData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const Profile = () => {
   const {profileId} = useParams()
   const [user, setUser] = useState(null)
-  const [post, setPost] = useState([])
+  const [posts, setPosts] = useState([])
+  const [activeTab, setActiveTab] = useState('posts')
+  const [showEdit, setShowEdit] = useState('false')
+
+  const fetchUser = async () => {
+    setUser(dummyUserData)
+    setPosts(dummyPostsData)
+  }
   
-  return (
-    <div>
+  useEffect(()=>{
+     fetchUser()
+  },[])
+
+  return user ? (
+    <div className='relative h-full overflow-y-scroll bg-gray-50 p-6'>
+      <div className='max-w-3xl mx-auto'>
+        {/* Profile Card */}
+        <div className='bg-white rounded-2xl shadow overflow-hidden'>
+          {/* Cover Photo */}
+          <div className='h-40 md:h-56 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200'>
+            {user.cover_photo && <img src={user.cover_photo} alt="" className='w-full h-full object-cover' /> }
+          </div>
+           {/* User Info */}
+        </div>
+
+      </div>
 
     </div>
-  )
+  ) : (<Loading/>)
 }
 
 export default Profile
