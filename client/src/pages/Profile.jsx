278ff1a63c9dfc6f98fbd18a68b6753b5a1c02d6
Shaheen <shn.sdq29@gmail.com>
2026-06-17 22:19:01 +0530

    tabs are added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import { dummyPostsData, dummyUserData } from '../assets/assets'
 import Loading from '../components/Loading'
+import UserProfileInfo from '../components/UserProfileInfo'
 
 const Profile = () => {
   const {profileId} = useParams()
@@ -29,8 +30,21 @@ const Profile = () => {
             {user.cover_photo && <img src={user.cover_photo} alt="" className='w-full h-full object-cover' /> }
           </div>
            {/* User Info */}
+           <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit} />
         </div>
 
+       {/* Tabs */}
+       <div className='mt-6'>
+         <div className='bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
+           {["posts","media","likes"].map((tab)=>(
+            <button onClick={()=>setActiveTab(tab)} key={tab} className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${activeTab === tab ? "bg-indigo-600 text-white" : "text-gray-600 hover:text-gray-900"}`}>
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+            </button>
+           ))}
+         </div>
+         {/* Posts */}
+         {}
+       </div>
       </div>
 
     </div>
