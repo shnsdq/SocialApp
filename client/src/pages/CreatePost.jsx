dd69eef7701055e8b411025b20f34d1e7069254b
Shaheen <shn.sdq29@gmail.com>
2026-06-18 23:21:24 +0530

    commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyUserData } from '../assets/assets'
 
 const CreatePost = () => {
+
+    const [content,setContent] = useState('')
+    const [images,setImages] = useState([])
+    const [loading,setLoading] = useState(false)
+
+    const user = dummyUserData;
+
   return (
-    <div>CreatePost</div>
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+        {/* Title */}
+        <div>
+          <h1>Create Post</h1>
+          <p>Share your thoughts with the world</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
