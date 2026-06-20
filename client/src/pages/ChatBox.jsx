da5b7c740b0982f19fe989455114256b5f8f9633
Shaheen <shn.sdq29@gmail.com>
2026-06-20 22:20:21 +0530

    profile section  added

--- client/src/pages/ChatBox.jsx
+++ client/src/pages/ChatBox.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { dummyMessagesData, dummyUserData } from '../assets/assets'
 
 const ChatBox = () => {
-  return (
-    <div>ChatBox</div>
+
+  const messages = dummyMessagesData
+  const [text, setText] = useState('')
+  const [image, setImage] = useState(null)
+  const [user, setUser] = useState(dummyUserData)
+  const messagesEndRef = useRef(null)
+
+  const sendMessage = async () => {
+    
+  }
+
+  useEffect(()=>{
+    messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
+  },[messages])
+ 
+  return user && (
+    <div className='flex flex-col h-screen'>
+      <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
+        <img src={user.profile_picture} alt="" className='size-8 rounded-full'/>
+        <div>
+          <p className='font-medium'>{user.full_name} </p>
+          <p className='text-sm text-gray-500 -mt-1.5'>@{user.username} </p>
+          </div> 
+      </div>
+       <div className='p-5 md:px-10 h-full overflow-y-scroll'>
+        <div>
+          
+        </div>
+       </div>
+    </div>
   )
 }
 
