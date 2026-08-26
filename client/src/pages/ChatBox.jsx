c78ce75d8ec3ce567eb9f4fae466ea613c62f12c
Shaheen <shn.sdq29@gmail.com>
2026-08-26 23:58:01 +0530

    Implement message sending functionality with image support and Redux integration

--- client/src/pages/ChatBox.jsx
+++ client/src/pages/ChatBox.jsx
@@ -1,17 +1,44 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { dummyMessagesData, dummyUserData } from '../assets/assets'
 import { ImageIcon, SendHorizonal } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { useAuth } from '@clerk/react'
+import api from '../api/axios'
 
 const ChatBox = () => {
 
-  const messages = dummyMessagesData
+  const messages = useSelector(state=>state.messages)
+  const {userId} = useParams()
+  const {getToken} = useAuth()
+  const dispatch = useDispatch()
+
   const [text, setText] = useState('')
   const [image, setImage] = useState(null)
-  const [user, setUser] = useState(dummyUserData)
+  const [user, setUser] = useState(null)
   const messagesEndRef = useRef(null)
 
   const sendMessage = async () => {
-    
+    try{
+        if(!text && !image) return
+
+        const token = await getToken()
+        const formData = new FormData()
+        formData.append('to_user_id', userId)
+        formData.append('text', text)
+        if(image) formData.append('image', image);
+
+        const {data} = await api.post('/api/message/send', formData, {
+            headers: {Authorization: `Bearer ${token}`}
+        })
+        if(data.success){
+          setText('')
+          setImage(null)
+          dispatch(addMessage(data.message))
+        }
+    }catch(error){
+
+    }
   }
 
   useEffect(()=>{
