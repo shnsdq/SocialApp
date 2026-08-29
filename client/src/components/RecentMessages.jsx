f28c05dca72144cd146d60b4263de17874a27a35
Shaheen <shn.sdq29@gmail.com>
2026-08-29 23:59:29 +0530

    Fetch and display recent messages with grouping and auto-refresh functionality

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -2,18 +2,51 @@ import React, { useEffect, useState } from 'react'
 import { dummyRecentMessagesData } from '../assets/assets'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import { useAuth, useUser } from '@clerk/react'
+import api from '../api/axios'
 
 const RecentMessages = () => {
 
     const [messages, setMessages] = useState([])
+    const {user} = useUser()
+    const {getToken} = useAuth()
+
      
     const fetchRecentMessages = async () => {
-        setMessages(dummyRecentMessagesData)
+        try {
+          const token = await getToken()
+          const {data} = await api.get('/api/user/recent-messages', {
+            headers: {Authorization:`Bearer ${token}`}
+          })
+          if(data.success){
+            //Group messages by sender and get the latest message for each sender
+            const groupedMessages = data.messages.reduce((acc, message) => {
+              const senderId = message.from_user_id._id;
+              if (!acc[senderId] || new Date(message.createdAt) > new Date(acc[senderId].createdAt)) {
+                acc[senderId] = message;
+              }
+              return acc;
+            }, {});
+
+            //SOrt messages by date
+            const sortedMessages = Object.values(groupedMessages).sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+            setMessages(sortedMessages);
+          }else{
+            toast.error(data.message)
+          }
+        } catch (error) {
+          toast.error(error.message)
+        }
     }
 
     useEffect(()=>{
-       fetchRecentMessages()
-    },[])
+      if(user){
+         fetchRecentMessages()
+         setInterval(fetchRecentMessages, 30000) //Fetch recent messages every 30 seconds
+         return () => {clearInterval()}
+      }
+      
+    },[user])
 
   return (
     <div className='bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs text-slate-800'>
@@ -21,7 +54,7 @@ const RecentMessages = () => {
      <div className='flex flex-col max-h-56 overflow-y-scroll no-scrollbar'>
       {
         messages.map((message,index)=>(
-          <Link to={`/messages/${message.from_user_id.id}`} key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
+          <Link to={`/messages/${message.from_user_id._id}`} key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
              <img src={message.from_user_id.profile_picture} alt="" className='w-8 h-8 rounded-full' />
              <div className='w-full'>
               <div className='flex justify-between'>
